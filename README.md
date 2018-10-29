f3371d70f18656699ea51f7d95498b9a550cdd23
Marc Saguer <saguermarc@gmail.com>
2018-10-29 09:40:52 +0100

    Jgitver init

---
+++ README.md
@@ -0,0 +1,57 @@
+# generator-jhipster-jgitver
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> JHipster module, Integration of JGitVer tool to jhipster project
+
+# Introduction
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://jhipster.github.io/installation.html)
+
+# Installation
+
+## With Yarn
+
+To install this module:
+
+```bash
+yarn global add generator-jhipster-jgitver
+```
+
+To update this module:
+
+```bash
+yarn global upgrade generator-jhipster-jgitver
+```
+
+## With NPM
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-jgitver
+```
+
+To update this module:
+
+```bash
+npm update -g generator-jhipster-jgitver
+```
+
+# Usage
+
+# License
+
+Apache-2.0 © [Marc Saguer](https://github.com/MSaguer)
+
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-jgitver.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-jgitver
+[travis-image]: https://travis-ci.org/MSaguer/generator-jhipster-jgitver.svg?branch=master
+[travis-url]: https://travis-ci.org/MSaguer/generator-jhipster-jgitver
+[daviddm-image]: https://david-dm.org/MSaguer/generator-jhipster-jgitver.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/MSaguer/generator-jhipster-jgitver
---
+++ generators/app/index.js
@@ -0,0 +1,144 @@
+const chalk = require('chalk');
+const packagejs = require('../../package.json');
+const semver = require('semver');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
+const jhipsterUtils = require('generator-jhipster/generators/utils');
+
+module.exports = class extends BaseGenerator {
+    get initializing() {
+        return {
+            init(args) {
+                if (args === 'default') {
+                    // do something when argument is 'default'
+                }
+            },
+            readConfig() {
+                this.jhipsterAppConfig = this.getAllJhipsterConfig();
+                if (!this.jhipsterAppConfig) {
+                    this.error('Can\'t read .yo-rc.json');
+                }
+            },
+            displayLogo() {
+                // it's here to show that you can use functions from generator-jhipster
+                // this function is in: generator-jhipster/generators/generator-base.js
+                // this.printJHipsterLogo();
+
+                // Have Yeoman greet the user.
+                this.log(`\nWelcome to the ${chalk.bold.yellow('JHipster jgitver')} generator! ${chalk.yellow(`v${packagejs.version}\n`)}`);
+            },
+            checkJhipster() {
+                const currentJhipsterVersion = this.jhipsterAppConfig.jhipsterVersion;
+                const minimumJhipsterVersion = packagejs.dependencies['generator-jhipster'];
+                if (!semver.satisfies(currentJhipsterVersion, minimumJhipsterVersion)) {
+                    this.warning(`\nYour generated project used an old JHipster version (${currentJhipsterVersion})... you need at least (${minimumJhipsterVersion})\n`);
+                }
+            }
+        };
+    }
+
+    writing() {
+        // function to use directly template
+        this.template = function (source, destination) {
+            this.fs.copyTpl(
+                this.templatePath(source),
+                this.destinationPath(destination),
+                this
+            );
+        };
+
+        // read config from .yo-rc.json
+        this.baseName = this.jhipsterAppConfig.baseName;
+        this.packageName = this.jhipsterAppConfig.packageName;
+        this.packageFolder = this.jhipsterAppConfig.packageFolder;
+        this.clientFramework = this.jhipsterAppConfig.clientFramework;
+        this.clientPackageManager = this.jhipsterAppConfig.clientPackageManager;
+        this.buildTool = this.jhipsterAppConfig.buildTool;
+
+        // use function in generator-base.js from generator-jhipster
+        this.angularAppName = this.getAngularAppName();
+
+        // use constants from generator-constants.js
+        const javaDir = `${jhipsterConstants.SERVER_MAIN_SRC_DIR + this.packageFolder}/`;
+        const resourceDir = jhipsterConstants.SERVER_MAIN_RES_DIR;
+        const webappDir = jhipsterConstants.CLIENT_MAIN_SRC_DIR;
+
+        // show all variables
+        this.log('\n--- some config read from config ---');
+        this.log(`baseName=${this.baseName}`);
+        this.log(`packageName=${this.packageName}`);
+        this.log(`clientFramework=${this.clientFramework}`);
+        this.log(`clientPackageManager=${this.clientPackageManager}`);
+        this.log(`buildTool=${this.buildTool}`);
+
+        this.log('\n--- some function ---');
+        this.log(`angularAppName=${this.angularAppName}`);
+
+        this.log('\n--- some const ---');
+        this.log(`javaDir=${javaDir}`);
+        this.log(`resourceDir=${resourceDir}`);
+        this.log(`webappDir=${webappDir}`);
+
+        this.updatePom = function () {
+            const pomFile = 'pom.xml';
+            try {
+                jhipsterUtils.replaceContent({
+                    file: pomFile,
+                    pattern: /(<groupId>com.mycompany.myapp<\/groupId>\n\s*<artifactId>samples<\/artifactId>\n\s*<version>)\S*(<\/version>)/m,
+                    regex: true,
+                    content: '$10.0.0$2'
+                }, this);
+            } catch (e) {
+                this.log(chalk.yellow('\nUnable to find ') + pomFile + chalk.yellow(' is not updated\n'));
+                this.debug('Error:', e);
+            }
+        };
+
+        this.jgitver_mavenLike = true;
+        this.jgitver_autoIncrementPatch = true;
+        this.jgitver_useCommitDistance = true;
+        this.jgitver_useDirty = true;
+
+        if (this.buildTool === 'maven') {
+            this.template('extensions.exml', '.mvn/extensions.xml');
+            this.template('jgitver.config.exml', '.mvn/jgitver.config.xml');
+            this.updatePom();
+        }
+        if (this.buildTool === 'gradle') {
+            this.error('Gradle option is not already implemented');
+        }
+    }
+
+    // install() {
+    //     let logMsg =
+    //         `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install`)}`;
+
+    //     if (this.clientFramework === 'angular1') {
+    //         logMsg =
+    //             `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install & bower install`)}`;
+    //     }
+    //     const injectDependenciesAndConstants = (err) => {
+    //         if (err) {
+    //             this.warning('Install of dependencies failed!');
+    //             this.log(logMsg);
+    //         } else if (this.clientFramework === 'angular1') {
+    //             this.spawnCommand('gulp', ['install']);
+    //         }
+    //     };
+    //     const installConfig = {
+    //         bower: this.clientFramework === 'angular1',
+    //         npm: this.clientPackageManager !== 'yarn',
+    //         yarn: this.clientPackageManager === 'yarn',
+    //         callback: injectDependenciesAndConstants
+    //     };
+    //     if (this.options['skip-install']) {
+    //         this.log(logMsg);
+    //     } else {
+    //         this.installDependencies(installConfig);
+    //     }
+    // }
+
+    end() {
+        this.log('End of jgitver generator');
+    }
+};
